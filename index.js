a59f620653c989b6b85a55479d675b363261a3a3
Todd Kennedy <todd@selfassembled.org>
2015-10-15 16:23:54 -0400

    updating tap & docker-remote-api to latest version

--- index.js
+++ index.js
@@ -1,80 +1,80 @@
-'use strict';
+'use strict'
 
-var Dr = require('docker-remote-api');
-var Promise = require('native-or-bluebird');
+var Dr = require('docker-remote-api')
+var Promise = require('native-or-bluebird')
 
-var dkr;
+var dkr
 
-function drPromised(method, url, opts) {
-  opts = opts || {};
-  var json = false;
+function drPromised (method, url, opts) {
+  opts = opts || {}
+  var json = false
 
   // using the json flag on docker-remote-api makes things hard so we'll
   // fix that
   if (opts.json) {
-    json = true;
-    delete opts.json;
+    json = true
+    delete opts.json
   }
 
   if (method === 'post' && !opts.body) {
-    opts.body = null;
+    opts.body = null
   }
 
-  return new Promise(function(resolve, reject) {
-    dkr[method].call(dkr, url, opts, function(err, stream) {
-      var statusRange = 2;
+  return new Promise(function (resolve, reject) {
+    dkr[method](url, opts, function (err, stream) {
+      var statusRange = 2
 
       if (stream && stream.statusCode) {
-        statusRange = Math.floor(stream.statusCode / 100);
+        statusRange = Math.floor(stream.statusCode / 100)
       }
 
       if (err || statusRange !== 2) {
-        return reject(err);
+        return reject(err)
       }
 
-      var data = [];
+      var data = []
 
       if (!stream) {
-        return resolve(true);
+        return resolve(true)
       }
 
       stream
-        .on('data', function(chunk) {
-          data.push(chunk.toString());
+        .on('data', function (chunk) {
+          data.push(chunk.toString())
         })
-        .on('error', function(err) {
-          reject(err);
+        .on('error', function (err) {
+          reject(err)
         })
-        .on('end', function(){
-          var resp = data.join('');
+        .on('end', function () {
+          var resp = data.join('')
 
           if (json) {
-            resp = JSON.parse(resp);
+            resp = JSON.parse(resp)
           }
 
-          resolve(resp);
-        });
-    });
-  });
+          resolve(resp)
+        })
+    })
+  })
 }
 
-module.exports = function(host) {
+module.exports = function (host) {
   if (!dkr) {
-    dkr = new Dr(host);
+    dkr = new Dr(host)
   }
 
   return {
-    post: function(url, opts){
-      return drPromised('post', url, opts);
+    post: function (url, opts) {
+      return drPromised('post', url, opts)
     },
-    get: function(url, opts){
-      return drPromised('get', url, opts);
+    get: function (url, opts) {
+      return drPromised('get', url, opts)
     },
-    delete: function(url, opts){
-      return drPromised('delete', url, opts);
+    delete: function (url, opts) {
+      return drPromised('delete', url, opts)
     },
-    put: function(url, opts){
-      return drPromised('put', url, opts);
+    put: function (url, opts) {
+      return drPromised('put', url, opts)
     }
-  };
-};
+  }
+}
--- test.js
+++ test.js
@@ -1,76 +1,79 @@
-'use strict';
+'use strict'
 
-var test = require('tap').test;
-var docker = require('./');
+var test = require('tap').test
+var docker = require('./')
 
-var dkr = docker(process.env.DOCKER_SOCK || '/var/run/docker.sock');
+var dkr = docker(process.env.DOCKER_SOCK || '/var/run/docker.sock')
 
-test('it posts', function(t) {
-  t.plan(2);
+test('it posts', function (t) {
+  t.plan(2)
   dkr
-    .post('/images/create?fromImage=busybox', {
+    .post('/images/create?fromImage=hello-world', {
       body: null
     })
-    .then(function(resp) {
-      t.type(resp, 'string', 'got back a string');
-      t.ok(/Download\ complete/.test(resp), 'download completed');
+    .then(function (resp) {
+      t.type(resp, 'string', 'got back a string')
+      var success = /Already\ exists/.test(resp) || /Download\ complete/.test(resp)
+      t.ok(success, 'download completed')
     })
-    .catch(function(err) {
-      t.notOk(!!err, err);
-    });
-});
-
-test('it gets', function(t) {
-  t.plan(1);
-  dkr
-    .get('/images/busybox/json')
-    .then(function(resp) {
-      t.ok(resp);
+    .catch(function (err) {
+      t.notOk(!!err, err)
     })
-    .catch(function(err) {
-      t.notOk(!!err, err);
-    });
-});
+})
 
-test('it parses json', function(t) {
-  t.plan(1);
+test('it gets', function (t) {
+  t.plan(1)
   dkr
-    .get('/images/busybox/json', {
+    .get('/images/hello-world/json')
+    .then(function (resp) {
+      t.ok(resp)
+    })
+    .catch(function (err) {
+      t.notOk(!!err, err)
+    })
+})
+
+test('it parses json', function (t) {
+  t.plan(1)
+  dkr
+    .get('/images/hello-world/json', {
       json: true
     })
-    .then(function(resp) {
-      t.type(resp, 'object', 'an object not a string');
+    .then(function (resp) {
+      t.type(resp, 'object', 'an object not a string')
     })
-    .catch(function(err) {
-      t.notOk(!!err, err);
-    });
-});
+    .catch(function (err) {
+      t.notOk(!!err, err)
+    })
+})
 
-test('it add body to posts that do not have one', function(t) {
-  t.plan(2);
+test('it add body to posts that do not have one', function (t) {
+  t.plan(2)
   dkr
-    .post('/images/create?fromImage=scratch')
-    .then(function(resp) {
-      t.type(resp, 'string', 'got back a string');
-      t.ok(/Download\ complete/.test(resp, 'download complete'));
+    .post('/images/create?fromImage=hello-world')
+    .then(function (resp) {
+      t.type(resp, 'string', 'got back a string')
+      var success = /Already\ exists/.test(resp) || /Download\ complete/.test(resp)
+      t.ok(success, 'success')
     })
-    .catch(function(err) {
-      t.notOk(!!err, err);
-    });
-});
+    .catch(function (err) {
+      t.notOk(!!err, err)
+    })
+})
 
-test('it handles when something has no response', function(t) {
-  t.plan(1);
+test('it handles when something has no response', function (t) {
+  t.plan(1)
 
-  var image = 'hello-world';
+  var imageName = `test${(new Date()).getTime()}`
+  var image = 'hello-world'
   var details = {
-    Hostname: "",
-    Domainname: "",
-    User: "",
+    Hostname: '',
+    Domainname: '',
+    User: '',
     Memory: 0,
     MemorySwap: 0,
     CpuShares: 512,
-    Cpuset: "0,1",
+    Cpuset: '0,1',
     AttachStdin: true,
     AttachStdout: true,
     AttachStderr: true,
@@ -79,13 +82,13 @@ test('it handles when something has no response', function(t) {
     StdinOnce: false,
     Env: null,
     Cmd: [],
-    Entrypoint: "",
+    Entrypoint: '',
     Image: image,
     Volumes: {},
-    WorkingDir: "",
+    WorkingDir: '',
     NetworkDisabled: false,
     ExposedPorts: {},
-    SecurityOpts: [""],
+    SecurityOpts: [''],
     HostConfig: {
       Binds: [],
       Links: [],
@@ -95,55 +98,55 @@ test('it handles when something has no response', function(t) {
       Privileged: false,
       ReadonlyRootfs: false,
       Dns: [],
-      DnsSearch: [""],
+      DnsSearch: [''],
       ExtraHosts: null,
       VolumesFrom: [],
       CapAdd: [],
       Capdrop: [],
       RestartPolicy: {
-        "Name": "",
-        "MaximumRetryCount": 0
+        'Name': '',
+        'MaximumRetryCount': 0
       },
-      NetworkMode: "bridge",
+      NetworkMode: 'bridge',
       Devices: []
     }
-  };
+  }
 
   var opts = {
     qs: {
-      name: 'test'
+      name: imageName
     },
     body: JSON.stringify(details),
     headers: {
       'content-type': 'application/json'
     }
-  };
+  }
 
   dkr
     .post('/images/create?fromImage=' + image)
-    .then(function() {
-      return dkr.post('/containers/create', opts);
+    .then(function () {
+      return dkr.post('/containers/create', opts)
     })
-    .then(function() {
-      return dkr.post('/containers/test/stop');
+    .then(function () {
+      return dkr.post(`/containers/${imageName}/stop`)
     })
-    .then(function(worked) {
-      t.ok(worked);
+    .then(function (worked) {
+      t.ok(worked)
     })
-    .catch(function(err) {
-      t.notOk(!!err, err);
-    });
-});
+    .catch(function (err) {
+      t.error(err, 'no errors caught')
+    })
+})
 
-test('it handles rejection', function(t) {
-  t.plan(2);
+test('it handles rejection', function (t) {
+  t.plan(2)
   dkr
     .post('/image/create?fromImage=zoopitydoo')
-    .then(function() {
-      t.ok(false, 'you should not pass');
+    .then(function () {
+      t.ok(false, 'you should not pass')
     })
-    .catch(function(err) {
-      t.ok(err, 'promise was rejected');
-      t.equal(err.status, 404, 'status was 404 for image');
-    });
-});
+    .catch(function (err) {
+      t.ok(err, 'promise was rejected')
+      t.equal(err.status, 404, 'status was 404 for image')
+    })
+})
